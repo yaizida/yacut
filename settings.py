506b8c7b0d91a736beff24e2824c7c5165dce383
Никита Бурковский <myizizda@gmail.com>
2024-01-17 14:04:49 +0300

    Вынужденная мера

--- settings.py
+++ settings.py
@@ -2,6 +2,7 @@ import os
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    # Почему то с getenv не прохидили тесты на платформе
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite3'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY')
